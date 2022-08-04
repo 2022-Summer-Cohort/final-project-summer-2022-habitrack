9485d08d52d13b0efe49725f83efef3c6e48314d
Chelsea Jo Calo <chelseajocalo@gmail.com>
2022-08-04 15:17:00 -0400

    header and logEntry js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,32 @@
 import header from "./header.js";
+import logEntry from "./logEntry.js";
+
+const container = document.querySelector(".container");
+container.innerHTML = header();
+container.innerHTML += logEntry();
+
+const button = document.querySelector(".dropbtn");
+button.addEventListener("click", ()=>{myFunction()});
+
+
+
+
+     
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      let dropdowns = document.getElementsByClassName("dropdown-content");
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
