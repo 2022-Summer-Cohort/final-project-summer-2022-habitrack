76d3e8fdbf43de398dc970ea62dd5e15c352457d
Chelsea Jo Calo <chelseajocalo@gmail.com>
2022-08-05 14:15:31 -0400

    end of day Friday

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,12 +1,77 @@
 import header from "./header.js";
 import logEntry from "./logEntry.js";
 import newHabit from "./newHabit.js";
+import login from "./login.js";
 
 const container = document.querySelector(".container");
-container.innerHTML = header();
-container.innerHTML += newHabit();
 
-const button = document.querySelector(".dropbtn");
+function makeLoginView(){
+  container.innerHTML = header();
+  container.innerHTML += login();
+  document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+})
+const createButton = document.querySelector("#create-btn") 
+createButton.addEventListener("click", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+    const password = document.querySelector("#create-password").value;
+    const confirmPassword = document.querySelector("#confirm-password").value;
+    const newUser = document.querySelector("#signupUsername").value;
+    
+if(newUser == ""){
+    alert("Please type in username")
+
+} else if (newUser.length < 10) {
+    alert("Username must be atleast 10 charecters long")
+
+} if (newUser.length >= 10){
+
+if (password == ""){
+        alert("Field cannot be empty.")
+    } else if (password != confirmPassword){
+        alert("Password did not match try again.")
+    } else if (password == confirmPassword){
+        alert("Account created")
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+    }
+
+
+}})
+const loginButton = document.querySelector("#login-button")
+loginButton.addEventListener("click", () => {
+    const username = document.querySelector("#login-username").value;
+    const userPassword = document.querySelector("#login-password").value;
+
+    if(username == ""){
+        alert("Please type in username")
+    } else if (userPassword == ""){
+            alert("Please type in Password")
+        }
+    else{
+       alert("Welcome back")
+    }
+   
+})
+}
+
+  makeLoginView();
+  const button = document.querySelector(".dropbtn");
 button.addEventListener("click", ()=>{myFunction()});
 
 
@@ -30,4 +95,3 @@ function myFunction() {
       }
     }
   }
-  
