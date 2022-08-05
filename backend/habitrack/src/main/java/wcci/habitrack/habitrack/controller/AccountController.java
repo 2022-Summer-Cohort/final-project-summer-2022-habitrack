d6abe7355f832e1a7c2a42d9a2ff16dc74d3c65c
Blake Printy <blakehprinty@gmail.com>
2022-08-05 10:12:10 -0400

    Added a postmapping for creating a new account

--- backend/habitrack/src/main/java/wcci/habitrack/habitrack/controller/AccountController.java
+++ backend/habitrack/src/main/java/wcci/habitrack/habitrack/controller/AccountController.java
@@ -1,6 +1,8 @@
 package wcci.habitrack.habitrack.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import wcci.habitrack.habitrack.model.Account;
 import wcci.habitrack.habitrack.repo.AccountRepository;
@@ -16,5 +18,11 @@ public class AccountController {
     @GetMapping("/api/accounts")
     public Iterable<Account> getAllAccounts() {
         return accountRepo.findAll();
+    }
+
+    @PostMapping("/api/accounts/newAccount")
+    public Account createNewAccount(@RequestBody Account newAccount) {
+        accountRepo.save(newAccount);
+        return newAccount;
     }
 }
